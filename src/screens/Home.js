527bfd4657bdd285dd3b95f422204dd77674a366
a.atalla <a.3talla@gmail.com>
2019-02-16 14:59:30 +0400

    add babel plugin-proposal-class-properties

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,13 +2,22 @@ import React from 'react';
 import Title from '../components/Title';
 import AtomIcon from '../assets/icons/atom-solid.svg';
 
-const Home = () => (
-  <div className="container">
-    <Title text="Smart Electron !!" />
-    <div className="icon-container">
-      <AtomIcon className="icon" />
-    </div>
-  </div>
-);
+class Home extends React.Component {
+  getName = () => {
+    console.log('Name');
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Title text="Smart Electron !!" />
+        <button onClick={this.getName}>Get Name</button>
+        <div className="icon-container">
+          <AtomIcon className="icon" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Home;
