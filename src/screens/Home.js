dffb460b451ad9fd5f1d58460c01b2887f92c279
a.atalla <a.3talla@gmail.com>
2019-02-16 15:00:29 +0400

    clean

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,22 +2,15 @@ import React from 'react';
 import Title from '../components/Title';
 import AtomIcon from '../assets/icons/atom-solid.svg';
 
-class Home extends React.Component {
-  getName = () => {
-    console.log('Name');
-  };
-
-  render() {
-    return (
-      <div className="container">
-        <Title text="Smart Electron !!" />
-        <button onClick={this.getName}>Get Name</button>
-        <div className="icon-container">
-          <AtomIcon className="icon" />
-        </div>
+function Home() {
+  return (
+    <div className="container">
+      <Title text="Smart Electron !!" />
+      <div className="icon-container">
+        <AtomIcon className="icon" />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Home;
