ebd75b15c3c5f41f6fdc7f08ae5b9d7862e3b7c1
a.atalla <a.3talla@gmail.com>
2019-02-16 17:02:12 +0400

    add (react & redux) devtools

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const { app, BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');
 
+if (isDev) {
+  const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
+  installExtension(REACT_DEVELOPER_TOOLS)
+    .then(name => console.log(`Added Extension:  ${name}`))
+    .catch(err => console.log('An error occurred: ', err));
+  installExtension(REDUX_DEVTOOLS)
+    .then(name => console.log(`Added Extension:  ${name}`))
+    .catch(err => console.log('An error occurred: ', err));
+}
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,8 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '~store/reducers/root-reducer';
 
+const isDev = process.env.NODE_ENV === 'development';
+
+/* eslint-disable-next-line no-underscore-dangle */
+const composeEnhancers = isDev ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+
 export default () => createStore(
   rootReducer,
-  applyMiddleware(thunk),
+  composeEnhancers(applyMiddleware(thunk)),
 );
